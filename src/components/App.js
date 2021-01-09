07389b27c20fd37321a5f38a65375aec7917ac60
TinTun123 <58549019+TinTun123@users.noreply.github.com>
2021-01-09 17:48:08 +0630

    simple netlify test near pig

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> nine pigs
+        <strong>{results.length}</strong> near pig
       </p>
       <hr></hr>
       {results
